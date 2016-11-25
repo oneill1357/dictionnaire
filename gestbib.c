0f8b59da4858a164d477e8e2a5b8d29f62405775
Anthony Clergeot <anthony.clergeot@gmail.com>
2016-11-25 11:03:38 +0100

    Correction menu
    Correction menu : exit

--- gestbib.c
+++ gestbib.c
@@ -11,13 +11,13 @@ void menu(){
         printf("1 - Creer un fichier dictionnaire\n");
         printf("2 - Utiliser un dictionnaire existant\n");
         printf("3 - Fabriquer un dictionnaire a partir d'un fichier texte\n");
-        printf("4 - Détruire un fichier dictionnaire\n");
+        printf("4 - Detruire un fichier dictionnaire\n");
         printf("5 - Inserer un mot dans un dictionnaire\n");
         printf("6 - Rechercher un mot dans un dictionnaire\n");
         printf("7 - Quitter\n");
 
         printf("\nChoix -> ");
-        scanf("\n%[^\n]", &choice);
+        scanf("\n%d", &choice);
     }
 
     switch (choice) {
